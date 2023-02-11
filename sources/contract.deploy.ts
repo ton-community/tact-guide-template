0ed50100833ae13f8031aab3454c4c43860230cf
AlexG <flexorise@gmail.com>
2023-02-12 01:30:07 +0400

    repair

--- sources/contract.deploy.ts
+++ sources/contract.deploy.ts
@@ -1,18 +1,16 @@
 import { beginCell, contractAddress, toNano, Address } from "ton";
-import { Counter } from "./output/sample_Counter";
+import { SampleTactContract, storeAdd } from "./output/sample_SampleTactContract";
 import { deploy } from "./utils/deploy";
 import { printAddress, printDeploy, printHeader } from "./utils/print";
 
 (async () => {
 
     // Parameters
-    const initialCounterValue = BigInt(Date.now());
-    let init = await Counter.init(initialCounterValue);
-
-    //let msg = beginCell().store(storeAdd({ $$type: 'Add', amount: 1n })).endCell(); // Replace if you want another message used
-    let msg = "Deploy";
+    let owner = Address.parse('kQDND6yHEzKB82ZGRn58aY9Tt_69Ie_uz73e2VuuJ3fVVcxf'); // Replace owner with your address
+    let packed = beginCell().store(storeAdd({ $$type: 'Add', amount: 1n })).endCell(); // Replace if you want another message used
+    let init = await SampleTactContract.init(owner);
     let address = contractAddress(0, init);
-    let deployAmount = toNano('0.2');
+    let deployAmount = toNano('0.5');
     let testnet = true;
 
     // Print basics
@@ -21,5 +19,8 @@ import { printAddress, printDeploy, printHeader } from "./utils/print";
     // printDeploy(init, deployAmount, packed, testnet);
     
     // Do deploy
-    await deploy(init, deployAmount, msg, testnet)
+    await deploy(init, deployAmount, packed, testnet)
 })();
+//
+// Address: kQAd00TX4YPxBfyWjArkionTZJVMoRzFQUM2ntQBcFycWYr4
+// Explorer: https://testnet.tonapi.io/account/kQAd00TX4YPxBfyWjArkionTZJVMoRzFQUM2ntQBcFycWYr4
